6cec5841172138f136801b76d887b356f83a78ab
Vincent Chho <vincec362@gmail.com>
2017-10-16 15:57:08 -0700

    Project finished.

---
+++ js/index.js
@@ -0,0 +1,40 @@
+$( document ).ready(function() {
+  // Main variables
+    // var $aboutTitle = $('.about-myself .content h2');
+    var $developmentWrapper = $('.development-wrapper');
+    var developmentIsVisible = false;
+
+
+  /* ####### HERO SECTION ####### */
+
+  $('.hero .content .header').delay(500).animate({
+    'opacity':'1',
+    'top': '50%'
+  },1000);
+
+  $('.target .sidenav .closebtn').delay(600).animate({
+    'opacity':'1',
+    'top': '2%'
+  },1300);
+
+  $('.hamburger').delay(600).animate({
+    'opacity':'1',
+    'top': '2%'
+  },1300);
+
+  $(window).scroll( function(){
+
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+
+
+  }); // -- End window scroll --
+});
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "200px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
---
+++ js/smoothscroll.js
@@ -0,0 +1,13 @@
+// Smooth Scrolling for internal links
+$('a[href^="#"]').on("click",function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $("html, body").stop().animate({
+	        "scrollTop": $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
